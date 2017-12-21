fa2aeccde8c246f9d9a0d73fbef3c3498a18e12e
sudheerj <jsudheer50@gmail.com>
2017-12-21 21:03:07 +0800

    QRCode component added

---
+++ components/qrcode/qrcode.component.ts
@@ -0,0 +1,75 @@
+import {
+    NgModule,
+    Component,
+    ElementRef,
+    OnDestroy,
+    Input,
+    Output,
+    EventEmitter,
+    OnInit,
+    ViewChild
+} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+declare var Qrious: any;
+
+@Component({
+    selector: 'pe-qrcode',
+    template: `<div class="ui-widget ui-corner-all">
+                <canvas #qrcode></canvas>
+                </div>`
+})
+export class QRCode implements OnDestroy, OnInit{
+
+    @Input() value: string;
+
+    @Input() background: string = 'white';
+
+    @Input() backgroundAlpha: number = 1.0;
+
+    @Input() foreground: string = 'black';
+
+    @Input() foregroundAlpha: number = 1.0;
+
+    @Input() level: string = 'L';
+
+    @Input() mime: string = 'image/png';
+
+    @Input() padding: number = null;
+
+    @Input() size: number = 100;
+
+    @ViewChild('qrcode') containerViewChild: ElementRef;
+
+    qrcode: null;
+
+    constructor(public el: ElementRef) {
+
+    }
+
+    ngOnInit() {
+        this.init();
+    }
+
+    init() {
+        let qrObject = {
+            element: this.containerViewChild.nativeElement,
+            background: this.background,
+            backgroundAlpha: this.backgroundAlpha,
+            foreground: this.foreground,
+            foregroundAlpha: this.foregroundAlpha,
+            level: this.level,
+            mime: this.mime,
+            padding: this.padding,
+            size: this.size,
+            value: this.value
+        };
+        this.qrcode = new Qrious(qrObject);
+    }
+
+    ngOnDestroy() {
+        this.qrcode = null;
+    }
+
+}
+

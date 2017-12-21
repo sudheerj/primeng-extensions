4054218f97d1e841350a831af0d50f8586bc7eb5
sudheerj <jsudheer50@gmail.com>
2017-12-21 21:14:41 +0800

    QRCode component fix

--- components/qrcode/qrcode.module.ts
+++ components/qrcode/qrcode.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {QRCode} from './qrcode.component';
 
 @NgModule({
-    imports: [CommonModule, QRCode],
+    imports: [CommonModule],
     exports: [QRCode],
     declarations: [QRCode]
 })

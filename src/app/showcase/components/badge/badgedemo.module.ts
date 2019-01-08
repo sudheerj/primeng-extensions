7371f0a937fc1c074ee59beec3aef0a9430c684a
sudheerj <jsudheer50@gmail.com>
2019-01-08 19:50:13 +0800

    Add individual imports instead global one

--- src/app/showcase/components/badge/badgedemo.module.ts
+++ src/app/showcase/components/badge/badgedemo.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule}    from '@angular/forms'
 import {BadgeDemo} from './badgedemo';
 import {BadgeDemoRoutingModule} from './badgedemo-routing.module';
-import {BadgeModule} from 'primeng-extensions/components/badge/badge';
+import {BadgeModule} from '../../../components/badge/badge';
 import {CodeHighlighterModule} from 'primeng/components/codehighlighter/codehighlighter';
 import {ButtonModule} from 'primeng/components/button/button';
 
---
+++ wizard.d.ts
@@ -0,0 +1,1 @@
+export * from './components/wizard/wizard';
